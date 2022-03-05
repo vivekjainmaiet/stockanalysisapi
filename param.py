2a44202a0d88d5461de0465ae8180e375c02b302
vivek jain <v.jain@iaea.org>
2022-03-05 18:30:36 +0100

    saving certificates and config for secure connection

--- param.py
+++ param.py
@@ -1,6 +1,12 @@
+from mysql.connector.constants import ClientFlag
+
 config = {
     'user': 'users',
     'password': '#Stocks@007#',
     'host': '34.79.163.70',
-    'database': 'stocksdb'
+    'database': 'stocksdb',
+    'client_flags': [ClientFlag.SSL],
+    'ssl_ca': 'ssl/server-ca.pem',
+    'ssl_cert': 'ssl/client-cert.pem',
+    'ssl_key': 'ssl/client-key.pem'
 }
