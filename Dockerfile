ed7586c6ca3f300c308f89723169d8b600ce34b7
vivek jain <v.jain@iaea.org>
2022-03-05 18:44:01 +0100

    saving ca to root

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,9 @@ COPY data.py data.py
 COPY param.py param.py
 COPY utils.py utils.py
 COPY requirements.txt requirements.txt
+COPY client-cert.pem client-cert.pem
+COPY client-key.pem client-key.pem
+COPY server-ca.pem server-ca.pem
 
 RUN pip install -U pip
 RUN pip install -r requirements.txt
--- param.py
+++ param.py
@@ -6,7 +6,7 @@ config = {
     'host': '34.79.163.70',
     'database': 'stocksdb',
     'client_flags': [ClientFlag.SSL],
-    'ssl_ca': 'ssl/server-ca.pem',
-    'ssl_cert': 'ssl/client-cert.pem',
-    'ssl_key': 'ssl/client-key.pem'
+    'ssl_ca': 'server-ca.pem',
+    'ssl_cert': 'client-cert.pem',
+    'ssl_key': 'client-key.pem'
 }
